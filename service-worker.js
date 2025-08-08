857a0929d8b78abc605edb84a110c8c9dd58faca
Axon App <axonapp@example.com>
2025-08-07 19:12:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,37 @@
+
+const CACHE_NAME = 'school-planner-cache-v1';
+const urlsToCache = [
+  '/',
+  '/SchoolPlanner/',
+  '/manifest.json',
+  '/index.html',
+  '/images/logoCalen192.png',
+  '/images/logoCalen512.png',
+];
+
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
 });
+
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
